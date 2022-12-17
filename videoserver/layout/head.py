50273210b8b51fd0d213ebfdf58d5725f20f399d
ntegan1 <ntegan1@gmail.com>
2022-12-17 17:23:55 -0500

    cleanup refactor tailwind
    rootdir
    
    add tailwind config but takes forever to load
    
    maybe better
    
    factor out ffmpeg
    
    folder for html layout web site stuff
    
    split out head
    
    removeroutespecificstuff

---
+++ videoserver/layout/head.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+def head():
+  return """
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <script src="https://cdn.tailwindcss.com"></script>
+      <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+              width: {
+              '1/8': '12.5%',
+              },
+              height: {
+              '1/8': '12.5%',
+              },
+              colors: {
+                clifford: '#da373d',
+                'invalidred': {
+                  DEFAULT: '#FF6B75',
+                },
+	  		  'nord0': '#2E3440',
+	  		  'nord1': '#3B4252',
+	  		  'nord2': '#434C5E',
+	  		  'nord3': '#4C566A',
+	  		  'nord4': '#D8DEE9',
+	  		  'nord5': '#E5E9F0',
+	  		  'nord6': '#ECEFF4',
+	  		  'nord7': '#8FBCBB',
+	  		  'nord8': '#88C0D0',
+	  		  'nord9': '#81A1C1',
+	  		  'nord10': '#5E81AC',
+	  		  'nord11': '#BF616A',
+	  		  'nord12': '#D08770',
+	  		  'nord13': '#EBCB8B',
+	  		  'nord14': '#A3BE8C',
+	  		  'nord15': '#B48EAD',
+              }
+            }
+          }
+        }
+      </script>
+    </head>
+    """
--- videoserver/main.py
+++ videoserver/main.py
@@ -1,94 +1,12 @@
 #!/usr/bin/env python3
-import os
-import subprocess
 
+from layout.head import head
+from util.route import segments_in_route, all_routes, all_segment_names, segment_to_segment_name, is_valid_segment
+from util.ffmpeg import ffmpeg_mp4_concat_wrap_process_builder, ffmpeg_mp4_wrap_process_builder
 from flask import Flask, Response, request
 #from selfdrive.loggerd.config import ROOT
-from selfdrive.loggerd.uploader import listdir_by_creation
-from tools.lib.route import SegmentName
 
-ROOT = "/home/ntegan/2022_12_16_tacov1/data/"
-
-def is_valid_segment(segment):
-  try:
-    segment_to_segment_name(ROOT, segment)
-    return True
-  except AssertionError:
-    return False
-
-def segment_to_segment_name(data_dir, segment):
-  fake_dongle = "ffffffffffffffff"
-  return SegmentName(str(os.path.join(data_dir, fake_dongle + "|" + segment)))
-
-def all_segment_names():
-  segments = []
-  for segment in listdir_by_creation(ROOT):
-    try:
-      segments.append(segment_to_segment_name(ROOT, segment))
-    except AssertionError:
-      pass
-  return segments
-
-def all_routes():
-  segment_names = all_segment_names()
-  route_names = [segment_name.route_name for segment_name in segment_names]
-  route_times = [route_name.time_str for route_name in route_names]
-  unique_routes = list(set(route_times))
-  return unique_routes
-
-def segments_in_route(route):
-  segment_names = [segment_name for segment_name in all_segment_names() if segment_name.time_str == route]
-  segments = [segment_name.time_str + "--" + str(segment_name.segment_num) for segment_name in segment_names]
-  return segments
-
-
-#TODO move to util/ffmpeg.py
-def ffmpeg_mp4_concat_wrap_process_builder(file_list, cameratype, chunk_size=1024*512):
-  command_line = ["ffmpeg"]
-  if not cameratype == "qcamera":
-    command_line += ["-f", "hevc"]
-  command_line += ["-r", "20"]
-  command_line += ["-i", "concat:" + file_list]
-  command_line += ["-c", "copy"]
-  command_line += ["-map", "0"]
-  if not cameratype == "qcamera":
-    command_line += ["-vtag", "hvc1"]
-  command_line += ["-f", "mp4"]
-  command_line += ["-movflags", "empty_moov"]
-  command_line += ["-"]
-  return subprocess.Popen(
-    command_line, stdout=subprocess.PIPE,
-    bufsize=chunk_size
-  )
-def ffmpeg_mp4_wrap_process_builder(filename):
-  """Returns a process that will wrap the given filename
-     inside an mp4 container, for easier playback by browsers
-     and other devices. Primary use case is streaming segment videos
-     to the vidserver tool.
-
-     filename is expected to be a pathname to one of the following
-       /path/to/a/qcamera.ts
-       /path/to/a/dcamera.hevc
-       /path/to/a/ecamera.hevc
-       /path/to/a/fcamera.hevc
-  """
-  basename = filename.rsplit("/")[-1]
-  extension = basename.rsplit(".")[-1]
-  command_line = ["ffmpeg"]
-  if extension == "hevc":
-    command_line += ["-f", "hevc"]
-  command_line += ["-r", "20"]
-  command_line += ["-i", filename]
-  command_line += ["-c", "copy"]
-  command_line += ["-map", "0"]
-  if extension == "hevc":
-    command_line += ["-vtag", "hvc1"]
-  command_line += ["-f", "mp4"]
-  command_line += ["-movflags", "empty_moov"]
-  command_line += ["-"]
-  return subprocess.Popen(
-    command_line, stdout=subprocess.PIPE
-  )
+ROOT = "/home/ntegan/2022_11_22_tomtp/"
 
 app = Flask(__name__,)
 
@@ -98,7 +16,7 @@ def full(cameratype, route):
   #if not is_valid_route(route):
   #  return "invalid route"
   file_name = cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
-  vidlist = "|".join(ROOT + "/" + segment + "/" + file_name for segment in segments_in_route(route))
+  vidlist = "|".join(ROOT + "/" + segment + "/" + file_name for segment in segments_in_route(ROOT, route))
   def generate_buffered_stream():
     with ffmpeg_mp4_concat_wrap_process_builder(vidlist, cameratype, chunk_size) as process:
       for chunk in iter(lambda: process.stdout.read(chunk_size), b""):
@@ -107,7 +25,7 @@ def full(cameratype, route):
 
 @app.route("/<cameratype>/<segment>")
 def fcamera(cameratype, segment):
-  if not is_valid_segment(segment):
+  if not is_valid_segment(ROOT, segment):
     return "invalid segment"
   file_name = ROOT + "/" + segment + "/" + cameratype + (".ts" if cameratype == "qcamera" else ".hevc")
 
@@ -127,10 +45,10 @@ def route(route):
 
   links = ""
   segments = ""
-  for segment in segments_in_route(route):
+  for segment in segments_in_route(ROOT, route):
     links += "<a href='"+route+"?"+segment.split("--")[2]+","+query_type+"'>"+segment+"</a><br>"
     segments += "'"+segment+"',"
-  return """<html>
+  return """<html>""" + head() + """
   <body>
     <video id="video" width="320" height="240" controls autoplay="autoplay" style="background:black">
     </video>
@@ -183,7 +101,7 @@ def route(route):
 @app.route("/")
 def index():
   result = ""
-  for route in all_routes():
+  for route in all_routes(ROOT):
     result += "<a href='"+route+"'>"+route+"</a><br>"
   return result
 
