bbdd5616dd6235e6231de3b7555e4c5a7a2cda95
ntegan1 <ntegan1@gmail.com>
2022-12-17 21:09:44 -0500

    Revert "draggable"
    This reverts commit d834499b4609265cc15b704515593710dc82d4c3.

--- videoserver/main.py
+++ videoserver/main.py
@@ -49,12 +49,8 @@ def route(route):
     links += "<a href='"+route+"?"+segment.split("--")[2]+","+query_type+"'>"+segment+"</a><br>"
     segments += "'"+segment+"',"
   content = """
-    <div class="flex w-full h-full">
-      <video id="video" class="bg-nord1 w-fit h-fit" width="320" height="240" controls autoplay="autoplay">
-      </video>
-      <div id="drag" class="draggable w-64 h-64 m-8 bg-nord10">
-      </div>
-    </div>
+    <video id="video" width="320" height="240" controls autoplay="autoplay" style="background:black">
+    </video>
     <br><br>
     current segment: <span id="currentsegment"></span>
     <br>
@@ -72,8 +68,6 @@ def route(route):
     """+links
   scripts="""
     <script>
-    document.getElementById('drag').DraggableJS();
-    document.getElementById('video').DraggableJS();
     var video = document.getElementById('video');
     var tracks = {
       list: ["""+segments+"""],
